d56de910a8f9ec442e7b0982f55446d21bc5d2a0
cvidi <clement.vidit.69100@outlook.fr>
2025-04-01 11:07:55 +0200

    Ajoutez des fichiers projet.

---
+++ WebMangasAspCore/Controllers/MangaController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebMangasAspCore.Models.MesExceptions;
+using WebMangasAspCore.Models.Dao;
+using WebMangasAspCore.Models.Metier;
+
+using WebMangasAspCore.Models.Persistance;
+
+namespace WebMangasAspCore.Controllers
+{
+    public class MangaController : Controller
+    {
+        public IActionResult Index()
+        {
+            System.Data.DataTable mesMangas = null;
+
+            try
+            {
+                mesMangas = ServiceManga.GetTousLesMangas();
+            }
+            catch (MonException e)
+            {
+                ModelState.AddModelError("Erreur", "Erreur lors de la récupération des mangas : " + e.Message);
+            }
+
+            return View(mesMangas);
+        }
+
+        public IActionResult Modifier(string id)
+        {
+            Manga unManga = null;
+            try
+            {
+                unManga = ServiceManga.GetunManga(id);
+                return View(unManga);
+            }
+            catch (MonException e)
+            {
+                return NotFound();
+            }
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Modifier(Manga unM)
+        {
+            try
+            {
+                ServiceManga.UpdateManga(unM);
+                return View();
+            }
+            catch (MonException e)
+            {
+                return NotFound();
+            }
+        }
+
+
+    }
+}
---
+++ WebMangasAspCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebMangasAspCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
